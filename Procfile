be6363c703c5de354f22c93bfc7f4015fde437c2
umair ansari <umairnsr87@gmail.com>
2020-04-03 11:52:14 +0530

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pucho_web.wsgi
---
+++ pucho_web/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for pucho_web project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pucho_web.settings')
+
+application = get_wsgi_application()
